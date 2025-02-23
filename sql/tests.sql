ea06fb0de0f0109b9ac52a5b30fcf168412242a1
Michael Toulouse <wrycoder@ameritech.net>
2025-02-23 18:14:20 +0000

    Cleaning up the reports

--- sql/tests.sql
+++ sql/tests.sql
@@ -1,18 +1,53 @@
+.headers off
+select "";
+select "";
+select "";
+select "BOOK AUTHORS";
+select "";
+.headers on
+.mode column
 select resources.title, users.name, signatures.role_id
  from resources
  inner join signatures on resources.id = signatures.resource_id,
             users on users.id = signatures.user_id
  where resources.category = 'book';
 
-select resources.title, recommendations.at, users.name
- from resources
- inner join recommendations on (resources.id = recommendations.resource_id),
-     users on (recommendations.disciple_id = users.id)
- order by recommendations.at;
-
+.headers off
+select "";
+select "";
+select "";
+select "WEBSITE PROMOTERS";
+select "";
 .headers on
 .mode column
+select resources.title, users.name, signatures.role_id
+ from resources
+ inner join signatures on resources.id = signatures.resource_id,
+            users on users.id = signatures.user_id
+ where resources.category = 'website';
 
+.headers off
+select "";
+select "";
+select "";
+select "BUSINESS PROMOTERS";
+select "";
+.headers on
+.mode column
+select resources.title, users.name, signatures.role_id
+ from resources
+ inner join signatures on resources.id = signatures.resource_id,
+            users on users.id = signatures.user_id
+ where resources.category = 'business';
+
+.headers off
+select "";
+select "";
+select "";
+select "RECOMMENDATION HISTORY REPORT";
+select "";
+.headers on
+.mode column
 select resources.title, advocates.name as advocate, disciples.name as disciple, recommendations.at
  from resources
  inner join recommendations on (recommendations.resource_id = resources.id),
